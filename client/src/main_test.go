fd6ef20cb69ee5f44865dd3a40b166f6e5e1daaa
jana <rathan.kparam@gmail.com>
2022-07-29 07:58:18 +0800

    Add more unit tests

--- client/src/main_test.go
+++ client/src/main_test.go
@@ -16,6 +16,7 @@ var givenVersion = util.CreateNumberPointer(0)
 
 func TestInit(t *testing.T) {
 	const baseUrl = "http://interview-accountapi:8080"
+	// const baseUrl = "http://localhost:8080"
 	config.SetBaseUrl(baseUrl)
 }
 
@@ -42,7 +43,7 @@ func TestCreate_expect201Created(t *testing.T) {
 	accountData.Attributes = &accountAttr
 
 	resp := operation.Create(accountData)
-	log.Println(resp)
+	// log.Println(resp)
 
 	const createdStatus = "201 Created"
 	var actualStatus = resp.Status
@@ -85,8 +86,81 @@ func TestCreate_expect409Conflict(t *testing.T) {
 	}
 }
 
-func TestOperations(t *testing.T) {
+func TestCreate_expect400BadRequest_missingField(t *testing.T) {
 
+	var accountData model.AccountData
+	accountData.ID = givenAccountId
+	// accountData.OrganisationID = "eb0bd6f5-c3f5-44b2-b677-acd23cdde616"
+	accountData.Type = "accounts"
+	accountData.Version = givenVersion
+
+	var accountAttr model.AccountAttributes
+	accountAttr.AccountClassification = util.CreateStringPointer("Personal")
+	accountAttr.AccountMatchingOptOut = util.CreateBooleanPointer(false)
+	accountAttr.AccountNumber = givenAccountNumber
+	accountAttr.AlternativeNames = []string{"Jana", "Rathan"}
+	accountAttr.BankID = "400300"
+	accountAttr.BankIDCode = "GBDSC"
+	accountAttr.BaseCurrency = "SGD"
+	accountAttr.Bic = "NWBKGB22"
+	accountAttr.Country = util.CreateStringPointer("SG")
+	accountAttr.Name = []string{"Jana", "Param"}
+
+	accountData.Attributes = &accountAttr
+
+	resp := operation.Create(accountData)
+	log.Println(resp)
+
+	const badRequestStatus = "400 Bad Request"
+	var actualStatus = resp.Status
+
+	if actualStatus != badRequestStatus {
+		t.Errorf("Operation status actual:%q expected:%q", actualStatus, badRequestStatus)
+	}
+}
+
+func TestCreate_expect400BadRequest_InvalidInput(t *testing.T) {
+
+	var accountData model.AccountData
+	accountData.ID = givenAccountId
+	accountData.OrganisationID = "eb0bd6f5-c3f5-44b2-b677-acd23cdde616"
+	accountData.Type = "accounts"
+	accountData.Version = givenVersion
+
+	var accountAttr model.AccountAttributes
+	accountAttr.AccountClassification = util.CreateStringPointer("Personal")
+	accountAttr.AccountMatchingOptOut = util.CreateBooleanPointer(false)
+	accountAttr.AccountNumber = givenAccountNumber
+	accountAttr.AlternativeNames = []string{"Jana", "Rathan"}
+	accountAttr.BankID = "400300"
+	accountAttr.BankIDCode = "GBDSC PQRS" // <--- incorrect format
+	accountAttr.BaseCurrency = "SGD"
+	accountAttr.Bic = "NWBKGB22"
+	accountAttr.Country = util.CreateStringPointer("SG")
+	accountAttr.Name = []string{"Jana", "Param"}
+
+	accountData.Attributes = &accountAttr
+
+	resp := operation.Create(accountData)
+	log.Println(resp)
+
+	const badRequestStatus = "400 Bad Request"
+	var actualStatus = resp.Status
+
+	if actualStatus != badRequestStatus {
+		t.Errorf("Operation status actual:%q expected:%q", actualStatus, badRequestStatus)
+	}
+}
+
+func TestFetch(t *testing.T) {
+	apiResponse := operation.Fetch()
+	accountDataList := apiResponse.AccountDataList
+	if len(accountDataList) != 1 {
+		t.Error("Expected one account")
+	}
+}
+
+func TestFetchMapped(t *testing.T) {
 	accounts := operation.FetchMapped()
 	// log.Println("AccountsAfterCreation:", accounts)
 
@@ -100,14 +174,20 @@ func TestOperations(t *testing.T) {
 	if fetchedAccountNumber != givenAccountNumber {
 		t.Errorf("AccountNumber expected:%q actual:%q", givenAccountNumber, fetchedAccountNumber)
 	}
+}
+
+func TestDelete(t *testing.T) {
 
 	operation.Delete(givenAccountId, *givenVersion)
 
-	// accountsAfterDeletion := operation.FetchMapped()
-	// log.Println("AccountsAfterDeletion:", accountsAfterDeletion)
+	accounts := operation.FetchMapped()
+	log.Println("AccountsAfterDeletion:", accounts)
 
-	if len(accounts) == 0 {
-		t.Error("Expected no accounts")
+	actualCount := len(accounts)
+	const expectedCount = 0
+
+	if actualCount != expectedCount {
+		t.Errorf("No of accounts actual:%q expected:%q", actualCount, expectedCount)
 	}
 
 }
